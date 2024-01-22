51bb44f74853b7b633d8cc415354b239e1b6c317
GoldenDragonDesigner <SokaGameDesign@gmail.com>
2024-01-22 16:31:13 -0700

    added pictures and updated database

---
+++ BeadsAndStrandInventory/Assets/Scripts/DataBaseScripts/EntityController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+public class EntityController : MonoBehaviour
+{
+    private BeadPricing_BeadInfo dbComponent;
+    private void Start() => dbComponent = GetComponent<BeadPricing_BeadInfo>();
+
+    //Assign first entity
+    public void AssignFirst() => Assign(0);
+    //Assign previous entity
+    public void AssignPrevious() => Assign(dbComponent.Entity == null ? 0 : dbComponent.Entity.Index - 1);
+    //Assign next entity
+    public void AssignNext() => Assign(dbComponent.Entity == null ? 0 : dbComponent.Entity.Index + 1);
+    //Assign last entity
+    public void AssignLast() => Assign(BeadPricing_BeadInfo.MetaDefault.CountEntities - 1);
+
+    private void Assign(int entityIndex)
+    {
+        //return if out of range
+        if (entityIndex < 0 || entityIndex >= BeadPricing_BeadInfo.MetaDefault.CountEntities) return;
+
+        dbComponent.Entity = BeadPricing_BeadInfo.MetaDefault.GetEntity(entityIndex);
+    }
+}
